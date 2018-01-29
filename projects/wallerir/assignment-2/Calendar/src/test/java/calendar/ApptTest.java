48c29888b89a8a7ea0b97fd7cf9f8379998ddca6
Ryan Wallerius <wallerir@flip3.engr.oregonstate.edu>
2018-01-29 09:23:51 -0800

    testing

--- projects/wallerir/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/wallerir/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -40,8 +40,32 @@ public class ApptTest {
 
 	 @Test
 	  public void test02()  throws Throwable  {
-		 
-	 }
+			 int startHour=8;
+			 int startMinute=15;
+			 int startDay=10;
+			 int startMonth=05;
+			 int startYear=2018;
+			 String title="Work Meeting";
+			 String description="Meeting for work.";
+			 //Construct a new Appointment object with the initial data
+			 Appt appt = new Appt(startHour,
+					 startMinute ,
+					 startDay ,
+					 startMonth ,
+					 startYear ,
+					 title,
+					 description);
+			 // assertions
+			 assertTrue(appt.getValid());
+			 assertEquals(8, appt.getStartHour());
+			 assertEquals(15, appt.getStartMinute());
+			 assertEquals(10, appt.getStartDay());
+			 assertEquals(05, appt.getStartMonth());
+			 assertEquals(2018, appt.getStartYear());
+			 assertEquals("Work Meeting", appt.getTitle());
+			 assertEquals("Meeting for Work.", appt.getDescription());
+
+		 }
 //add more unit tests as you needed
 	
 }
