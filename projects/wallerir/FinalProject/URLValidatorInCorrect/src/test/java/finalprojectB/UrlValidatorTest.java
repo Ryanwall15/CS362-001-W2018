edc64fc6f59e33cece54ab071f61e4ffde741a87
Ryan Wallerius <wallerir@flip1.engr.oregonstate.edu>
2018-03-19 14:31:44 -0700

    Manual and Input Partitioning tests

--- projects/wallerir/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/wallerir/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -3,6 +3,8 @@ package finalprojectB;
 
 import junit.framework.TestCase;
 
+import java.net.URL;
+
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
@@ -13,28 +15,62 @@ import junit.framework.TestCase;
 
 public class UrlValidatorTest extends TestCase {
 
+   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
 
    public UrlValidatorTest(String testName) {
       super(testName);
    }
 
    
-   
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+//You can use this function to implement your manual testing
+      assertNotNull(urlVal);
+      assertTrue(urlVal.isValid("http://www.google.com/")); //Valid URL
+      //assertTrue(urlVal.isValidAuthority("www.google.com")); //Potential bug
+
+      assertFalse(urlVal.isValid("234piojad;")); //Invalid URL
+
 	   
    }
    
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+	 //You can use this function to implement your First Partition testing
+
+      assertTrue(urlVal.isValidScheme("h3p"));
+      assertTrue(urlVal.isValidScheme("htp"));
+      assertTrue(urlVal.isValidScheme("https"));
+      assertTrue(urlVal.isValidScheme("http"));
+
+      assertFalse(urlVal.isValidScheme("://"));
+      assertFalse(urlVal.isValidScheme(":/"));
 
    }
    
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+		 //You can use this function to implement your Second Partition testing
+
+    assertFalse(urlVal.isValidAuthority(null));
+
+    //There seems to be a bug with testing the authority
+      //Says regualr expression expected but I'm giving it a valid authority
+    //assertTrue(urlVal.isValidAuthority("https://www.google.com"));
+    //assertFalse(urlVal.isValidAuthority("1.3.4.5"));
+      // assertFalse(urlVal.isValidAuthority(""));
+
+      //Path Testing
+      assertTrue(urlVal.isValidPath(""));
+      assertTrue(urlVal.isValidPath("/file"));
+      assertFalse(urlVal.isValidPath(null));
+      assertFalse(urlVal.isValidPath("/.."));
+
+      //Query Testing
+      assertTrue(urlVal.isValidQuery(""));
+      assertTrue(urlVal.isValidQuery("a;lksdfj"));
+      assertFalse(urlVal.isValidQuery("a b"));
 
    }
    //You need to create more test cases for your Partitions if you need to 
