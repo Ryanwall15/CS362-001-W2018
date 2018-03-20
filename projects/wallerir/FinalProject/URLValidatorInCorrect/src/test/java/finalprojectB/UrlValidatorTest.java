aab79c6117df01d5bf2ae54490348f402ce4ee30
Ryan Wallerius <wallerir@flip1.engr.oregonstate.edu>
2018-03-19 19:57:57 -0700

    Added final test

--- projects/wallerir/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/wallerir/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -1,29 +1,28 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package finalprojectB;
-
 import junit.framework.TestCase;
-
-import java.net.URL;
-
-//You can use this as a skeleton for your 3 different test approach
-//It is an optional to use this file, you can generate your own test file(s) to test the target function!
-// Again, it is up to you to use this file or not!
-
-
-
-
+import org.junit.Test;
 
 public class UrlValidatorTest extends TestCase {
-
    UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
-
-   public UrlValidatorTest(String testName) {
-      super(testName);
-   }
-
-   
-   public void testManualTest()
+   @Test
+   public void testManualTest() throws Throwable
    {
 //You can use this function to implement your manual testing
       assertNotNull(urlVal);
@@ -34,13 +33,13 @@ public class UrlValidatorTest extends TestCase {
       assertFalse(urlVal.isValid("234piojad;")); //Invalid URL
       //assertFalse(urlVal.isValid("htt://www.github.com"));
 
-	   
+
    }
-   
-   
-   public void testYourFirstPartition()
+
+   @Test
+   public void testYourFirstPartition() throws Throwable
    {
-	 //You can use this function to implement your First Partition testing
+      //You can use this function to implement your First Partition testing
 
       assertTrue(urlVal.isValidScheme("h3p"));
       assertTrue(urlVal.isValidScheme("htp"));
@@ -51,16 +50,18 @@ public class UrlValidatorTest extends TestCase {
       assertFalse(urlVal.isValidScheme(":/"));
 
    }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing
 
-    assertFalse(urlVal.isValidAuthority(null));
+   @Test
+   public void testYourSecondPartition() throws Throwable
+   {
+      //You can use this function to implement your Second Partition testing
 
-    //There seems to be a bug with testing the authority
+      assertFalse(urlVal.isValidAuthority(null));
+
+      //There seems to be a bug with testing the authority
       //Says regualr expression expected but I'm giving it a valid authority
-    //assertTrue(urlVal.isValidAuthority("https://www.google.com"));
-    //assertFalse(urlVal.isValidAuthority("1.3.4.5"));
+      //assertTrue(urlVal.isValidAuthority("https://www.google.com"));
+      //assertFalse(urlVal.isValidAuthority("1.3.4.5"));
       // assertFalse(urlVal.isValidAuthority(""));
 
       //Path Testing
@@ -75,14 +76,31 @@ public class UrlValidatorTest extends TestCase {
       assertFalse(urlVal.isValidQuery("a b"));
 
    }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
+   //You need to create more test cases for your Partitions if you need to
 
+
+   //didn't know how much he wanted us to do. Objective vague, specifications unclear. Confused about what we are supposed to accomplish.
+   @Test
+   public void testBasicProgramming1()  throws Throwable  {
+      String[] schemesCheck = {"http", "hhtp", "file"};
+      boolean[] validity = {true, false, false};
+      String[] schemes = {"Mkat", "http"};
+      UrlValidator myURL = new UrlValidator(schemes, 0);
+      for (int i = 0; i < 3; i++) {
+         assertEquals(schemesCheck[i], validity[i], myURL.isValidScheme((schemesCheck[i])));
+      }
    }
-   
-
-
+   @Test
+   public void testBasicProgramming2()  throws Throwable {
+      String[] URLs = {"http://www.google.com:80/test1?action=view",
+              "http/www.google.com:80/test1?action=view",
+              "ftp://255.255.255.255:65535/t123",
+              "ftp://255.255.255.25565535/t123?aktion=view",
+              "file://255.255.255.255:65535/t123?aktion=view",
+      };
+      boolean[] validity = {true, false, true, false, false};
+      for (int i = 0; i < 5; i++) {
+         assertEquals(URLs[i], validity[i], urlVal.isValid(URLs[i]));
+      }
+   }
 }
